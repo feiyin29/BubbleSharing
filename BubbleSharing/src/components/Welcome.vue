f30f837eb49c52de4166a47328547280eb0e9433
Anchistha Tinchatarak <anchistha45@st.ac.th>
2022-12-12 15:06:47 +0700

    add vuetify & welcome page

---
+++ BubbleSharing/src/components/Welcome.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-row>
+        hi
+        <v-btn variant="outlined"> Button </v-btn>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup></script>
---
+++ BubbleSharing/src/views/WelcomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Welcome from '../components/Welcome.vue'
+</script>
+
+<template>
+  <main>
+    <Welcome />
+  </main>
+</template>
