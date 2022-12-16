e0c7729df60a6581dbd7e7757ca93696be90e2f4
Anchistha Tinchatarak <anchistha45@st.ac.th>
2022-12-16 22:54:21 +0700

    myPage + welcome not done

---
+++ BubbleSharing - Copy/src/stores/products.js
@@ -0,0 +1,43 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue'
+export const useProductStore = defineStore({
+  id: 'products',
+  state: () => ({
+    countNumber: 1,
+    products: [
+      // {
+      //   title: "Pencil",
+      //   image: 'src/assets/pencil.png',
+      //   url: 'www.google.com',
+      // },
+      // {
+      //   title: "Eraser",
+      //   image: 'src/assets/eraser.png',
+      //   url: 'www.youtube.com',
+      // },
+    ],
+  }),
+  actions: {
+    pushData(param) {
+      this.products.push(param);
+      console.log(this.products);
+    },
+    incrementNumber() {
+      this.countNumber++;
+      console.log(this.countNumber);
+    },
+    updateByEdit(item) {
+      const find = this.products.find((each, index) => {
+        console.log(index)
+        console.log(item.index)
+
+        if (index == item.index.value) return each
+      })
+      find.title = item.title.value
+      find.image = item.image.value
+      find.url = item.url.value
+      this.products.splice(item.index.value, 1, find)
+    },
+    
+  },
+});
---
+++ BubbleSharing - Copy/src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <v-col cols="8" class="ma-10 font-serif">
+            <p class="text-8xl text-yellow-400"> jn test </p>
+
+        </v-col>
+    <h1 class="text-3xl font-bold ">This is an about page</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
